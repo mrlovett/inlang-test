4e47ae17089abefaf1ee8bb82253df2e2ef78169
Morgan Lovett <34556357+mrlovett@users.noreply.github.com>
2023-05-02 23:20:26 +0100

    Update inlang.config.js

--- inlang.config.js
+++ inlang.config.js
@@ -1,14 +1,21 @@
 export async function defineConfig(env) {
-	const { pluginJson } = await env.$import(
-		"https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js",
-	)
+  const plugin = await env.$import(
+    "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
+  );
 
-	return {
-		referenceLanguage: "en",
-		plugins: [
-			pluginJson({
-				pathPattern: "translations/{language}.json",
-			}),
-		],
-	}
+  const pluginConfig = {
+    pathPattern: "./{language}.json",
+  };
+
+  return {
+    referenceLanguage: "en",
+    languages: await plugin.getLanguages({
+      ...env,
+      pluginConfig,
+    }),
+    readResources: (args) =>
+      plugin.readResources({ ...args, ...env, pluginConfig }),
+    writeResources: (args) =>
+      plugin.writeResources({ ...args, ...env, pluginConfig }),
+  };
 }
